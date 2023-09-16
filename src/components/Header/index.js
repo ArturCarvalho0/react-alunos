006fef062527269cb9cb87c1fd4ec6dbba1da837
Artur Carvalho <artur.carvalho0@outlook.com>
2023-09-16 10:32:15 -0300

    Páginas e rotas da aplicação

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,19 +5,17 @@ import { useSelector } from 'react-redux';
 import { Nav } from './styled';
 
 export default function Header() {
-  const botaoClicado = useSelector((state) => state.example.botaoClicado);
   return (
     <Nav>
       <Link to="/">
         <FaHome size={24} />
       </Link>
-      <Link to="/login">
+      <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/aaaaaa">
+      <Link to="/login">
         <FaSignInAlt size={24} />
       </Link>
-      {botaoClicado ? 'clicado': 'não clicado'}
     </Nav>
   );
 }
